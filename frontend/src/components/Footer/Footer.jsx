6b3f3fc74f2444f987c6fccabca7b57597ff6b9e
vishal5208 <vishald5208@gmail.com>
2023-07-11 16:57:25 +0530

    added routing

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
 							Provides comprehensive coverage for your furry friend.
 						</p>
 					</div>
-					<div className="w-full md:w-1/2 lg:w-1/3 mb-6 md:mb-0">
+					<div className="w-full md:w-1/2 lg:w-1/3 mb-6 md:mb-0 flex flex-col justify-center items-center">
 						<h4 className="text-xl font-bold text-white mb-4">Quick Links</h4>
-						<ul className="text-white">
+						<ul className="text-white flex flex-col items-center justify-center	">
 							<li className="mb-2">About Us</li>
 							<li className="mb-2">Contact Us</li>
 						</ul>
